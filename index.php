505e4a331c1aa714adf40adebde15f3f0969e238
aliffauzi34 <56574544+aliffauzi34@users.noreply.github.com>
2020-01-04 20:10:02 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>SCRIPT BY ALIFFAUZI34</title>
+</head>
+<body>
+	<form class="search" method="POST" action="proses.php">
+		<input type="text" class="form-control" name="uid" placeholder="Masukkan User ID"><br><br>
+		<input type="text" class="form-control" name="token" placeholder="Masukkan token"><br>
+		<div class="form-group">
+			<label>PILIH Kuotanya</label>
+			<select name="kuota" id="kuota" class="form-control">
+				<option value="1000">1000 an</option>
+				<option value="5000">5000 an</option>
+			</select>
+		</div><br>
+		<button type="submit" name="masuk">Masuk mas</button>
 
+	</form>
+</body>
+</html>
